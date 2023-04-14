274c96e11abc488fc3c1aeeecb83edb5ac421c05
iantony254 <iantonyochola@gmail.com>
2023-04-14 14:21:34 +0300

    shelly test

--- shell.c
+++ shell.c
@@ -1,19 +1,26 @@
+/**
+ *This implementation reads user input using fgets and removes the trailing newline character using strcspn. 
+ *It then forks a child process to execute the command using execlp, which searches for the command in the PATH environment variable and executes it with any arguments passed in as additional parameters. 
+ *If execlp fails, it prints an error message using perror and exits the child process with EXIT_FAILURE. 
+ *The parent process waits for the child to exit using waitpid and prints any error messages using perror.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_CMD_LENGTH 1024
+#define MAX_CLI_LENGTH 1024
 
 int main(void) {
-  char cmd[MAX_CMD_LENGTH];
+  char cmd[MAX_CLI_LENGTH];
   int status;
 
   while (1) {
     // Display prompt and read user input
     printf("#the_matrix$ ");
-    if (fgets(cmd, MAX_CMD_LENGTH, stdin) == NULL) {
+    if (fgets(cmd, MAX_CLI_LENGTH, stdin) == NULL) {
       printf("\n");
       break; // End of file condition (Ctrl+D)
     }
---
+++ shelly.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 1024
+
+int main(void) {
+    char input[MAX_INPUT_LENGTH];
+    char *args[2];
+
+    while (1) {
+        // Display prompt
+
+        // Read input from user
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
+            // Handle end of file condition
+            printf("\n");
+            break;
+        }
+
+        // Remove trailing newline character
+        input[strcspn(input, "\n")] = '\0';
+
+        // Tokenize input into command and arguments
+        args[0] = input;
+        args[1] = NULL;
+
+        // Fork process to execute command
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+            execvp(args[0], args);
+            // If execvp returns, there was an error
+            printf("Error: command not found\n");
+            exit(EXIT_FAILURE);
+        } else {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+    return 0;
+}
