e80e8e4c7645cfd9046efcd7a2405b1ff6fbd085
iantony254 <iantonyochola@gmail.com>
2023-04-14 14:34:27 +0300

    shelly test correction

--- shelly.c
+++ shelly.c
@@ -12,6 +12,7 @@ int main(void) {
 
     while (1) {
         // Display prompt
+        printf("th3_m@tr!x_$ ");
 
         // Read input from user
         if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
