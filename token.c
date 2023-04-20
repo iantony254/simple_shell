2d5c4c353e39a8ff9a0df170f4b30a4427822686
iantony254 <iantonyochola@gmail.com>
2023-04-21 02:23:02 +0300

    token.c done

--- token.c
+++ token.c
@@ -4,15 +4,20 @@
 #define MAX_ARGS 10
 #define MAX_ARG_LENGTH 1024
 
-int tokenize(char *input, char **args) {
+int tokenize(char *input, char **args)
+{
     int argc = 0;
 
-    // Tokenize input into command and arguments
+    /*Tokenize input into command and arguments*/
+
     char *token = strtok(input, " ");
-    while (token != NULL) {
-        if (argc >= MAX_ARGS) {
-            // Too many arguments
-            return -1;
+
+    while (token != NULL)
+    {
+        if (argc >= MAX_ARGS)
+        {
+            /*Too many arguments*/
+            return (-1);
         }
         args[argc] = token;
         argc++;
