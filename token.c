30d4ebc41416bf210012121596cdb4b15801c3f6
iantony254 <iantonyochola@gmail.com>
2023-04-21 02:28:11 +0300

    token.c done

--- token.c
+++ token.c
@@ -6,24 +6,22 @@
 
 int tokenize(char *input, char **args)
 {
-    int argc = 0;
+	int argc = 0;
 
-    /*Tokenize input into command and arguments*/
+	/*Tokenize input into command and arguments*/
+	char *token = strtok(input, " ");
 
-    char *token = strtok(input, " ");
-
-    while (token != NULL)
-    {
-        if (argc >= MAX_ARGS)
-        {
-            /*Too many arguments*/
-            return (-1);
-        }
-        args[argc] = token;
-        argc++;
-        token = strtok(NULL, " ");
-    }
-    args[argc] = NULL;
-
-    return argc;
+	while (token != NULL)
+	{
+		if (argc >= MAX_ARGS)
+		{
+			/*Too many arguments*/
+			return (-1);
+		}
+		args[argc] = token;
+		argc++;
+		token = strtok(NULL, " ");
+	}
+	args[argc] = NULL;
+	return argc;
 }
