5eccf5453ef2890058bf7f62825276cdea083fa6
iantony254 <iantonyochola@gmail.com>
2023-04-21 03:32:32 +0300

    update 5

--- shelly.h
+++ shelly.h
@@ -1,16 +1,18 @@
 #ifndef SHELLY_H
 #define SHELLY_H
 
-/**
- * File: shelly.h
- * Desc: Header file containing prototypes for all functions
- */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "token.c"
+/**
+ * File: shelly.h
+ * Desc: Header file containing prototypes for all functions
+ */
+
+
+
 
 int tokenize(char *input, char **args);
 
