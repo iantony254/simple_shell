26096a25618cb3004a65c8ed4d06488f596935d4
iantony254 <iantonyochola@gmail.com>
2023-04-24 20:37:22 +0300

    shell 2.0 redone

--- shell.h
+++ shell.h
@@ -1,13 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-char **parse_command(char *input);
 void execute_command(char *command, char *path);
 
 #endif
