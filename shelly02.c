84ec8a8c484468defcf712a83990853dd42809a8
GraceWere <gwere2000@gmail.com>
2023-04-25 10:18:42 -0700

    update shelly02.c

--- shelly02.c
+++ shelly02.c
@@ -4,61 +4,90 @@
 #define MAX_INPUT_LENGTH 1024
 #define MAX_ARGS 64
 
+/**
+ * display_prompt - Display shell prompt
+ */
+
+void display_prompt(void)
+{
+printf("th3_m@tr!x_$ ");
+}
+
+/**
+ * read_input - Read input from user
+ * @input: Pointer to buffer where input will be stored
+ */
+
+void read_input(char *input)
+{
+if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+{
+printf("\n");
+exit(EXIT_SUCCESS);
+}
+input[strcspn(input, "\n")] = '\0';
+}
+
+/**
+ * tokenize_input - Tokenize input into command and arguments
+ * @input: Pointer to input string
+ * @args: Array of pointers to arguments
+ */
+
+void tokenize_input(char *input, char **args)
+{
+char *token = strtok(input, " ");
+int i = 0;
+while (token != NULL && i < MAX_ARGS - 1)
+{
+args[i++] = token;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
+}
+
+/**
+ * execute_command - Fork process to execute command
+ * @args: Array of pointers to arguments
+ */
+
+void execute_command(char **args)
+{
+pid_t pid = fork();
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+execvp(args[0], args);
+printf("Error: command not found\n");
+exit(EXIT_FAILURE);
+}
+else
+{
+int status;
+waitpid(pid, &status, 0);
+}
+}
+
+/**
+ * main - Entry point of the shell program
+ *
+ * Return:Always (0)
+ */
+
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH];
-	char *args[MAX_ARGS];
-
-	while (1)
-	{
-		/*Display prompt*/
-		printf("th3_m@tr!x_$ ");
-
-		/*Read input from user*/
-		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
-		{
-			/*Handle end of file condition*/
-			printf("\n");
-			break;
-		}
-		/* Remove trailing newline character*/
-		input[strcspn(input, "\n")] = '\0';
-
-		/*Tokenize input into command and arguments*/
-		char *token 
-		*token = strtok(input, " ");
-		int i = 0;
-		while (token != NULL && i < MAX_ARGS - 1)
-		{
-			args[i] = token;
-			i++;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
-
-		/*Fork process to execute command*/
-		pid_t pid = fork();
-
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			/*Child process*/
-			execvp(args[0], args);
-			/*If execvp returns, there was an error*/
-			printf("Error: command not found\n");
-			exit(EXIT_FAILURE);
-			}
-		else
-		{
-			/*Parent process*/
-			int status;
-
-			waitpid(pid, &status, 0);
-		}
-	}
-	return (0);
+char input[MAX_INPUT_LENGTH];
+char *args[MAX_ARGS];
+while (1)
+{
+display_prompt();
+read_input(input);
+tokenize_input(input, args);
+execute_command(args);
+}
+return (0);
 }
