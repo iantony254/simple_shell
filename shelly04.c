9f168687b62638e85de18f975510fdeb286d3ce1
GraceWere <gwere2000@gmail.com>
2023-04-26 05:52:32 -0700

    update shelly04.c

--- shelly04.c
+++ shelly04.c
@@ -1,84 +1,109 @@
 #include "shelly.h"
 #include <sys/wait.h>
 #include "token.c"
-
+#include "execute.c"
 
 #define MAX_INPUT_LENGTH 1024
 
-extern char **environ;  /* Declare external environ variable */
+/**
+ * handle_exit_command - prints a message and exits the program
+ * Return: none
+ */
+
+void handle_exit_command(void)
+{
+printf("Exiting the matrix terminal...\n");
+exit(EXIT_SUCCESS);
+}
+
+/**
+ * handle_env_command - prints the environment variables
+ * Return: none
+ */
+
+void handle_env_command(void)
+{
+char **env = environ;
+while (*env)
+{
+printf("%s\n", *env);
+env++;
+}
+}
+
+/**
+ * fork_and_execute - forks the process and executes the given command
+ * @args: an array of strings containing the command and its arguments
+ *
+ * Return: none
+ */
+
+void fork_and_execute(char **args)
+{
+pid_t pid = fork();
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+/*Child process*/
+execute_command(args);
+}
+else
+{
+/*Parent process*/
+int status;
+waitpid(pid, &status, 0);
+}
+}
+
+/**
+ * main - the main function that runs the program
+ * Return:0 on success, otherwise an error code
+ */
 
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH];
-	char *args [MAX_ARGS + 1];
+char input[MAX_INPUT_LENGTH];
+char *args[MAX_ARGS + 1];
 
-	while (1)
-	{
-		/*Display prompt*/
-		printf("th3_m@tr!x_$: ");
-
-		/*Read input from user*/
-		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
-		{
-			/*Handle end of file condition*/
-			printf("\n");
-			break;
-		}
-		/* Remove trailing newline character*/
-		input[strcspn(input, "\n")] = '\0';
-
-		/* Check if the input given is the "exit" command */
-		if (strcmp(input, "exit") == 0)
-		{
-			/* Exit The Program */
-			printf("Exiting the matrix terminal...\n");
-			exit(EXIT_SUCCESS);
-		}
-
-		/* Check if the input given is the "env" command */
-		if (strcmp(input, "env") == 0)
-		{
-			/* Print Environment Variables */
-			char **env = environ;
-			while (*env)
-			{
-				printf("%s\n", *env);
-				env++;
-			}
-			continue;
-		}
-		
-		/*Tokenize input into command and arguments*/
-		int argc = tokenize(input, args);
-		if (argc == -1)
-		{
-			printf("Error: Too many Arguments!\n");
-			continue;
-		}
-		
-		/*Fork process to execute command*/
-		pid_t pid = fork();
-
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			/*Child process*/
-			execvp(args[0], args);
-			/*If execvp returns, there was an error*/
-			printf("Error: command not found\n");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			/*Parent process*/
-			int status;
-
-			waitpid(pid, &status, 0);
-		}
-	}
-	return (0);
+while (1)
+{
+/*Display prompt*/
+printf("th3_m@tr!x_$: ");
+/*Read input from user*/
+if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+{
+/*Handle end of file condition*/
+printf("\n");
+break;
+}
+/* Remove trailing newline character*/
+input[strcspn(input, "\n")] = '\0';
+/* Check if the input given is the "exit" command */
+if (strcmp(input, "exit") == 0)
+{
+/* Exit The Program */
+handle_exit_command();
+}
+/* Check if the input given is the "env" command */
+if (strcmp(input, "env") == 0)
+{
+/* Print Environment Variables */
+handle_env_command();
+continue;
+}
+/*Tokenize input into command and arguments*/
+int argc = tokenize(input, args);
+if (argc == -1)
+{
+printf("Error: Too many Arguments!\n");
+continue;
+}
+/*Fork process to execute command*/
+fork_and_execute(args);
+}
+return (0);
 }
