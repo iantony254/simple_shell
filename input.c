c1fd665e7af1eb1768b2499c827b5f0ede726212
iantony254 <iantonyochola@gmail.com>
2023-04-26 22:57:43 +0300

    emotional damage 2

---
+++ input.c
@@ -0,0 +1,24 @@
+#include "shelly.h"
+
+#define MAX_INPUT_LENGTH 1024
+
+/**
+ * read_input - Reads input from stdin and removes trailing newline character.
+ *
+ * @input: Pointer to a buffer where the input will be stored.
+ *
+ * Return: 1 if input was read successfully, 0 otherwise.
+ */
+
+int read_input(char *input)
+{
+if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+{
+/* Handle end of file condition */
+printf("\n");
+return (0);
+}
+/* Remove trailing newline character */
+input[strcspn(input, "\n")] = '\0';
+return (1);
+}
---
+++ shelly.h
@@ -0,0 +1,39 @@
+#ifndef _SHELLY_H_
+#define _SHELLY_H_
+
+/*
+ * File: shelly.h
+ * Desc: Header file containing prototypes for all functions
+ *       written in the simple_shell directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+int tokenize(char *input, char **args);
+
+void display_prompt(void);
+int read_input(char *input);
+void tokenize_input(char *input, char **args, int *argc);
+void execute_command(char **args);
+void handle_child_process(char **args);
+void handle_parent_process(pid_t pid);
+
+void handle_exit_command(void);
+void handle_env_command(void);
+void fork_and_execute(char **args);
+
+void handle_input(char input[]);
+void get_input(char *input);
+void parse_input(char *input, char **args);
+void wait_for_child(pid_t pid);
+
+#endif /* _SHELLY_H_ */
