4dbfdb68025aa1cf8ac099083aa04f50bc0e6859
iantony254 <iantonyochola@gmail.com>
2023-04-27 03:52:31 +0300

    final test

--- shell.h
+++ shell.h
@@ -12,21 +12,14 @@ extern char **environ;  /* Declare external environ variable */
 
 void display_prompt(void);
 int read_input(char *input);
-int tokenize(char *input, char **args);
-int execute_command(char **args);
 void tokenize_input(char *input, char **args, int *argc);
-void execute_command(char **args);
+int execute_command(char **args);
 void handle_child_process(char **args);
-void handle_parent_process(pid_t pid);
-
 void handle_exit_command(void);
 void handle_env_command(void);
-void fork_and_execute(char **args);
-
 void handle_input(char input[]);
 void get_input(char *input);
 void parse_input(char *input, char **args);
-void wait_for_child(pid_t pid);
 /* void print_aliases(void); */
 /* void print_alias(char *name); */
 /* void set_alias(char *name, char *value); */
