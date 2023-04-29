476c145ae94c7741874261a0f0ff8ddfa830ded4
GraceWere <gwere2000@gmail.com>
2023-04-28 17:31:41 -0700

    last test

---
+++ shell.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#include "shell.h"
+
+void display_prompt(void) {
+    printf("$ ");
+}
+
+char* read_input(void) {
+    char* line = NULL;
+    size_t bufsize = 0; 
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+char** parse_input(char* line) {
+    int bufsize = 64, position = 0;
+    char** tokens = malloc(bufsize * sizeof(char*));
+    char* token;
+
+    if (!tokens) {
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += 64;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, " \t\r\n\a");
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int execute_command(char** args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+      /* Child process*/
+      
+        if (execvp(args[0], args) == -1) {
+            perror("Error");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+      /* Error forking */
+      perror("Error");
+    } else {
+      /* Parent process*/
+
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status) && wpid != -1);
+    }
+
+    return 1;
+}
+
+void free_args(char** args) {
+    int i = 0;
+    while (args[i] != NULL) {
+        free(args[i]);
+        i++;
+    }
+    free(args);
+}
+
+int main() {
+    char *input;
+    char **args;
+    int status;
+
+    do {
+        display_prompt();
+        input = read_input();
+        args = parse_input(input);
+        status = execute_command(args);
+
+        free(input);
+        free_args(args);
+    } while (status);
+
+    return 0;
+}
---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+/* Constants */
+#define BUFSIZE 1024
+#define TOK_DELIM " \t\r\n\a"
+
+/* Function prototypes */
+void display_prompt(void);
+char *read_input(void);
+char **parse_input(char *input);
+int execute_command(char **args);
+int _strlen(char *str);
+void free_args(char **args);
+
+void perror(const char *s);
+void exit(int status);
+
+#endif
