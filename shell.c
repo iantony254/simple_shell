9519e1fe49f14bda96fa8eb0bf207569f1b684b7
GraceWere <gwere2000@gmail.com>
2023-04-28 18:43:53 -0700

    small changes

--- shell.c
+++ shell.c
@@ -54,17 +54,17 @@ int execute_command(char** args) {
 
     pid = fork();
     if (pid == 0) {
-        /* Child process */
+      /* Child process*/
 
         if (execvp(args[0], args) == -1) {
             perror("Error");
         }
         exit(EXIT_FAILURE);
     } else if (pid < 0) {
-        /* Error forking */
-        perror("Error");
+      /* Error forking */
+      perror("Error");
     } else {
-        /* Parent process */
+      /* Parent process*/
 
         do {
             wpid = waitpid(pid, &status, WUNTRACED);
