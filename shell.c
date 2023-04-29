a689c9bbe0e0b3820a6e90b4eebaaa9167c2f456
GraceWere <gwere2000@gmail.com>
2023-04-29 05:17:21 +0300

    modify shell.c

--- shell.c
+++ shell.c
@@ -8,119 +8,93 @@
 #include "shell.h"
 
 void display_prompt(void) {
-    printf("$ ");
+printf("$ ");
 }
 
 char* read_input(void) {
-    char* line = NULL;
-    size_t bufsize = 0;
-    getline(&line, &bufsize, stdin);
-    return line;
+char* line = NULL;
+size_t bufsize = 0;
+getline(&line, &bufsize, stdin);
+return line;
 }
 
 char** parse_input(char* line) {
-    int bufsize = 64, position = 0;
-    char** tokens = malloc(bufsize * sizeof(char*));
-    char* token;
+int bufsize = 64, position = 0;
+char** tokens = malloc(bufsize * sizeof(char*));
+char* token;
 
-    if (!tokens) {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+if (!tokens) {
+fprintf(stderr, "Allocation error\n");
+exit(EXIT_FAILURE);
+}
 
-    token = strtok(line, " \t\r\n\a");
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
+token = strtok(line, " \t\r\n\a");
+while (token != NULL) {
+tokens[position] = token;
+position++;
 
-        if (position >= bufsize) {
-            bufsize += 64;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "Allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
+if (position >= bufsize) {
+bufsize += 64;
+tokens = realloc(tokens, bufsize * sizeof(char*));
+if (!tokens) {
+fprintf(stderr, "Allocation error\n");
+exit(EXIT_FAILURE);
+}
+}
 
-        token = strtok(NULL, " \t\r\n\a");
-    }
-    tokens[position] = NULL;
-    return tokens;
+token = strtok(NULL, " \t\r\n\a");
+}
+tokens[position] = NULL;
+return tokens;
 }
 
 int execute_command(char** args) {
-    pid_t pid, wpid;
-    int status;
-    char* path = getenv("PATH");
-    char* token;
-    char* command_path = NULL;
+pid_t pid, wpid;
+int status;
 
-    /* Search for the command in the directories listed in the PATH environment variable*/
-    token = strtok(path, ":");
-    while (token != NULL) {
-        char* temp_path = malloc(strlen(token) + strlen(args[0]) + 2);
-        sprintf(temp_path, "%s/%s", token, args[0]);
-        if (access(temp_path, X_OK) == 0) {
-            command_path = temp_path;
-            break;
-        }
-        free(temp_path);
-        token = strtok(NULL, ":");
-    }
+pid = fork();
+if (pid == 0) {
+/* Child process*/
 
-    if (command_path == NULL) {
-      /* Command not found*/
-        fprintf(stderr, "Command not found: %s\n", args[0]);
-        return 1;
-    }
-
-    pid = fork();
-    if (pid == 0) {
-        /* Child process */
-
-        if (execve(command_path, args, NULL) == -1) {
-            perror("Error");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        /* Error forking */
-        perror("Error");
-    } else {
-        /* Parent process */
-
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status) && wpid != -1);
-    }
-
-    free(command_path);
-    return 1;
+if (execvp(args[0], args) == -1) {
+perror("Error");
 }
+exit(EXIT_FAILURE);
+} else if (pid < 0) {
+/* Error forking */
+perror("Error");
+} else {
+/* Parent process*/
 
-
+do {
+wpid = waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status) && wpid != -1);
+}
+return 1;
+}
 void free_args(char** args) {
-    int i = 0;
-    while (args[i] != NULL) {
-        free(args[i]);
-        i++;
-    }
-    free(args);
+int i = 0;
+while (args[i] != NULL) {
+free(args[i]);
+i++;
+}
+free(args);
 }
 
 int main() {
-    char *input;
-    char **args;
-    int status;
+char *input;
+char **args;
+int status;
 
-    do {
-        display_prompt();
-        input = read_input();
-        args = parse_input(input);
-        status = execute_command(args);
+do {
+display_prompt();
+input = read_input();
+args = parse_input(input);
+status = execute_command(args);
 
-        free(input);
-        free_args(args);
-    } while (status);
+free(input);
+free_args(args);
+} while (status);
 
-    return 0;
+return 0;
 }
